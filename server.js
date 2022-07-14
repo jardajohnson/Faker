46dcbce0730a536988267ed5708e419fd366f633
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-14 16:00:19 -0700

    add get user and company get routes

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
+const { faker } = require('@faker-js/faker');
+
 const express = require('express');
 const app = express();
 
+// ! Middleware
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+class User {
+	constructor() {
+		this._id = faker.database.mongodbObjectId();
+		this.firstName = faker.name.firstName();
+		this.lastName = faker.name.lastName();
+		this.email = faker.internet.email();
+		this.phoneNumber = faker.phone.number();
+		this.password = faker.internet.password();
+	}
+}
+
+class Company {
+	constructor() {
+		this._id = faker.database.mongodbObjectId();
+		this.name = faker.company.companyName();
+		this.address = {
+			street: faker.address.street(),
+			city: faker.address.city(),
+			state: faker.address.state(),
+			zip: faker.address.zipCode(),
+			country: faker.address.country(),
+		};
+	}
+}
+
 // req is short for request
 // res is short for response
-app.get('/api', (req, res) => {
-	res.send('Our express api server is now sending this over to the browser');
+app.get('/api/users/new', (req, res) => {
+	res.json(new User());
+});
+app.get('/api/companies/new', (req, res) => {
+	res.json(new Company());
+});
+app.get('/api/user/company', (req, res) => {
+	res.json([new User(), new Company()]);
 });
 
 const server = app.listen(8080, () =>
